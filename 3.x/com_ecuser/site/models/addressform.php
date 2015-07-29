2a5380756d4eec09a4f494355dcb8452e9707bb1
seokjoon <seokjoon@gmail.com>
2015-07-29 21:55:18 +0900

    user form

--- 3.x/com_ecuser/site/models/addressform.php
+++ 3.x/com_ecuser/site/models/addressform.php
@@ -7,4 +7,14 @@ defined('_JEXEC') or die('Restricted access');
 
 class EcuserModelAddressform extends EcModelForm	{
 	
+	public function getItem($keyValue = null)	{ //EcDebug::log(__method__);
+		$valueKey = JFactory::getApplication()->input->get('user', 0, 'uint');
+		$this->setState($this->nameKey, $valueKey);
+		return parent::getItem($keyValue);
+	}
+
+	public function getTable($type = 'User', $prefix = 'EcuserTable', $config = array()) {
+		return JTable::getInstance($type, $prefix, $config);
+	}
+	
 }
--- 3.x/com_ecuser/site/models/profileform.php
+++ 3.x/com_ecuser/site/models/profileform.php
@@ -8,12 +8,18 @@ defined('_JEXEC') or die('Restricted access');
 class EcuserModelProfileform extends EcModelForm	{
 	
 	public function getItem($keyValue = null)	{ //EcDebug::log(__method__);
-		$item = parent::getItem($keyValue); 
+		$valueKey = JFactory::getApplication()->input->get('user', 0, 'uint');
+		$this->setState($this->nameKey, $valueKey);
+		$item = parent::getItem($keyValue);
 		if(empty($item)) return $item;
 		if(($this->getState('joinUser')) && ($item->user > 0)) {
 			$table = $this->getTable('User', 'JTable');
 			$table->load($item->user); //EcDebug::log($table);
 			$item->name = $table->name; } //EcDebug::lp($item);
 		return $item;
+	}
+
+	public function getTable($type = 'User', $prefix = 'EcuserTable', $config = array()) {
+		return JTable::getInstance($type, $prefix, $config);
 	}
 }
