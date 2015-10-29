1605a26c7e08a15b64af763622b9f0882264093e
seokjoon <seokjoon@gmail.com>
2015-10-29 18:36:57 +0900

    .

--- 3.x/com_ec/admin/models/fields/usr.php
+++ 3.x/com_ec/admin/models/fields/usr.php
@@ -1,16 +1,16 @@
 <?php /** @package ecfirm.net
 * @copyright	Copyright (C) kilmeny.net. All rights reserved.
-* @license GNU General Public License version 2 or later. */
+* @license GNU General Public License version 2 or later. *
 defined('_JEXEC') or die('Restricted access');
 JFormHelper::loadFieldClass('list');
 
 
 
-class JFormFieldCont extends JFormFieldList	{
+class JFormFieldUsr extends JFormFieldList {
 	/** * The form field type.
 	 * @var		string
 	 * @since   1.6 */
-	protected $type = 'Cont';
+	protected $type = 'Usr';
 	
 	/** * Method to get the field options.
 	 * @return  array  The field option objects.
@@ -19,9 +19,9 @@ class JFormFieldCont extends JFormFieldList	{
 		$options = array();
 		$db = JFactory::getDbo();
 		$query = $db->getQuery(true)
-			->select('c.cont as value, c.title as text') 
-			->from('#__ec_cont as c')
-			->order('c.title');
+			->select('ju.id as value, ju.username as text')
+			->from('#__users as ju')
+			->order('ju.username');
 		$db->setQuery($query);
 		try { $options = $db->loadObjectList(); } 
 		catch (RuntimeException $e) { JError::raiseWarning(500, $e->getMessage()); }
