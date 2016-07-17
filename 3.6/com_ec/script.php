e173ac16fc1152b48da04a420eb7d85f7102ee57
seokjoon <seokjoon@gmail.com>
2016-07-17 15:58:36 +0900

    .

---
+++ 3.6/com_ec/script.php
@@ -0,0 +1,17 @@
+<?php /** @package ecfirm.net
+* @copyright	Copyright (C) ecfirm.net. All rights reserved.
+* @license GNU General Public License version 2 or later. */
+defined('_JEXEC') or die('Restricted access');
+
+
+
+class Com_EcInstallerScript {
+	
+	public function install($parent) { }
+
+	public function uninstall($parent) { }
+	
+	public function preflight($type, $parent) { }
+	
+	public function postflight($type, $parent) { }
+}
