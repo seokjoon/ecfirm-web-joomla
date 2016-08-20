0516b2882b3d2ceee67a54e78ba87965e6ec21b5
seokjoon <seokjoon@gmail.com>
2016-08-20 12:33:21 +0900

    토픽 항목 첨부 이미지/파일 디스플레이

--- 3.6/com_ectopic/site/views/topic/tmpl/default_topic.php
+++ 3.6/com_ectopic/site/views/topic/tmpl/default_topic.php
@@ -5,7 +5,6 @@ defined('_JEXEC') or die('Restricted access');
 
 
 
-
 echo '<fieldset><legend>'.$title.'</legend>';
 	echo '<div class="pull-left span5">';
 		echo '<div class="center">'.$modified.$seperator.$hits.$topiccmt.$topiclike.$numberFile.$numberImg.'</div>';
@@ -15,3 +14,10 @@ echo '<fieldset><legend>'.$title.'</legend>';
 	echo '</div><div class="clearfix"></div>';
 	echo '<div style="border: solid 1px #dddddd; padding: 10px;">'.nl2br($item->body).'</div>';
 echo '</fieldset>';
+
+
+
+if($existImg) echo '<div align="center" style="margin: 10px;"><a href="'
+	.JUri::base().$imgs['img'].'"><img class="media-object thumbnail" src="'
+	.JUri::base().$imgs['imgthumb'].'" alt=""></a></div>';
+if($existFile) echo '<div><a href="'.JUri::base().$files['file'].'">'.basename($files['file']).'</a></div>';
