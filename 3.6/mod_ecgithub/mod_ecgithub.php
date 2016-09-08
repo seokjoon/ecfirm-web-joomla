53d1ff5c0c4462cd40c83d084300dc1e963dd494
seokjoon <seokjoon@gmail.com>
2016-09-08 09:45:40 +0900

    mod_ecgithub template

---
+++ 3.6/mod_ecgithub/mod_ecgithub.php
@@ -0,0 +1,10 @@
+<?php /** @package ecfirm.net
+ * @copyright	Copyright (C) ecfirm.net. All rights reserved.
+ * @license GNU General Public License version 2 or later. */
+defined('_JEXEC') or die('Restricted access');
+
+
+
+require_once dirname(__FILE__).'/helper.php';
+
+require JModuleHelper::getLayoutPath('mod_ecgithub', $params->get('layout', 'default'));
